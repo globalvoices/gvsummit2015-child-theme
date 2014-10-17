62cfd0b531f1672e0829900388264b78469062f0
jeremyclarke <jer@simianuprising.com>
2014-10-17 17:20:55 -0400

    initial commit of style.css with new turqoise, vestigial /images/ dir and geo-mashup files

---
+++ geo-mashup-info-window.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Geo Mashup plugin's 'info-window.php' templage
+ * 
+ * Copied from the /default-templates/ folder in the plugin. 
+ * 
+ * For styling of the info window, see map-style-default.css.
+ *
+ * @package GeoMashup
+ */
+
+include(get_template_directory() . "/geo-mashup-info-window.php");
